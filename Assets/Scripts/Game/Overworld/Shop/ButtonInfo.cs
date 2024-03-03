a202d9906a387d949fd568875cb417e341dc19f0
Dean-Jensen <jensendean42@gmail.com>
2024-03-03 14:25:59 -0800

    Squashed commit of the following:
    commit 03ff01f8947ae59fadae59f6eeb9001af64c2e9c
    Author: poderman <powderbilly2007@gmail.com>
    Date:   Sat Mar 2 19:27:44 2024 -0800
    
        Shop added to game
    
    commit 44b1139965f0ad3751977662a91a60443bd34b9b
    Merge: b146345 4596637
    Author: Dean-Jensen <95256624+Dean-Jensen@users.noreply.github.com>
    Date:   Fri Mar 1 09:10:18 2024 -0800
    
        Merge branch 'main' into New-Branch
    
    commit b146345c154c299f6133be313493188b2a538df6
    Author: poderman <powderbilly2007@gmail.com>
    Date:   Thu Feb 29 11:39:55 2024 -0800
    
        hi
    
    commit 38a6565ea72738091c9d9f0a3603f1f842c49f59
    Merge: b0b432c 2a0213c
    Author: poderman <powderbilly2007@gmail.com>
    Date:   Thu Feb 29 11:27:42 2024 -0800
    
        Merge branch 'New-Branch' of https://github.com/Dean-Jensen/UAAG into New-Branch
    
    commit b0b432ce8d1f3c7222c2d51ceb1c239808549322
    Author: poderman <powderbilly2007@gmail.com>
    Date:   Thu Feb 29 11:27:17 2024 -0800
    
        Sword Combat
    
    commit 2a0213c3d03a519da941e123f71810b45426fecb
    Author: poderman <powderbilly2007@gmail.com>
    Date:   Thu Feb 29 11:17:34 2024 -0800
    
        overworld
    
    commit cb10edee1c5646abda599105f40262e1e475e964
    Author: poderman <powderbilly2007@gmail.com>
    Date:   Thu Feb 29 10:33:59 2024 -0800
    
        Level design of 2nd level
    
        Requires a jump magnitude of 1450 to beat it meaning the player will have to upgrade jump to be able to beat it

--- Assets/Scripts/Game/Overworld/Shop/ButtonInfo.cs
+++ Assets/Scripts/Game/Overworld/Shop/ButtonInfo.cs
@@ -14,17 +14,22 @@ public class ButtonInfo : MonoBehaviour
     public TMP_Text PriceTxt; //This is the price text on the button
     public TMP_Text QuantityTxt; //This is the quantity of the item text
     public GameObject ShopManager; //This is the ShopManager object
-    private ShopManager _shopManager;
+    //private ShopManager _shopManager;
 
     private void Start()
     {
-        _shopManager = ShopManager.GetComponent<ShopManager>();
+        //_shopManager = ShopManager.GetComponent<ShopManager>();
     }
 
     void Update()
     {
         //This will update both the price and quantity text on the botton
+        PriceTxt.text = "Price: $" + ShopManager.GetComponent<ShopManager>().shopItems[2, ItemID].ToString();
+        QuantityTxt.text = ShopManager.GetComponent<ShopManager>().shopItems[3, ItemID].ToString();
+
+        /*
+        //This will update both the price and quantity text on the botton
         PriceTxt.text = "Price: $" + _shopManager.shopItems[2, ItemID];
-        QuantityTxt.text = _shopManager.shopItems[3, ItemID].ToString();
+        QuantityTxt.text = _shopManager.shopItems[3, ItemID].ToString();*/
     }
 }
